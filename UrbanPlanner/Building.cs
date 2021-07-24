23027646305e11669c0ae8db33a1e8de30f1cb99
Hunter Juneau <huntlee17@gmail.com>
2021-07-24 00:42:15 -0500

    Add project files.

---
+++ UrbanPlanner/Building.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrbanPlanner
+{
+    class Building
+    {
+        private string _designer;
+        private DateTime _dateConstructed;
+        private string _address;
+        private string _owner;
+
+        public byte Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume => 3 * Stories * Width * Depth;
+
+        public Building(string address)
+        {
+            _address = address;
+        }
+
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+        }
+
+        public void Purchase(string purchaser)
+        {
+            _owner = purchaser;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"{_address}---------------");
+            Console.WriteLine($"Designed by {_designer}");
+            Console.WriteLine($"Constructed on {_dateConstructed}");
+            Console.WriteLine($"Owned by {_owner}");
+            Console.WriteLine($"{Volume} cubic meters of space");
+        }
+    }
+}
---
+++ UrbanPlanner/Program.cs
@@ -0,0 +1,35 @@
+﻿using static UrbanPlanner.Building;
+using System;
+
+namespace UrbanPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building KeangnamHanoiLandmarkTower = new Building("Khu E6 Đô thị mới Cầu Giấy, Phạm Hùng, Mễ Trì, Nam Từ Liêm, Hà Nội, Vietnam");
+            KeangnamHanoiLandmarkTower.Stories = 72;
+            KeangnamHanoiLandmarkTower.Width = 368;
+            KeangnamHanoiLandmarkTower.Depth = 125;
+            KeangnamHanoiLandmarkTower.Construct();
+            KeangnamHanoiLandmarkTower.Purchase("Keangnam Enterprises, Ltd.");
+            KeangnamHanoiLandmarkTower.DisplayInfo();
+
+            Building MakkahRoyalClockTower = new Building("الملاوي، Al Haram, Mecca 24231, Saudi Arabia");
+            MakkahRoyalClockTower.Stories = 120;
+            MakkahRoyalClockTower.Width = 557;
+            MakkahRoyalClockTower.Depth = 557;
+            MakkahRoyalClockTower.Construct();
+            MakkahRoyalClockTower.Purchase("Saudi Binladin Group");
+            MakkahRoyalClockTower.DisplayInfo();
+
+            Building ChongqingWorldFinancialCenter = new Building("China, Chongqing, Yuzhong District, 民族路188号");
+            ChongqingWorldFinancialCenter.Stories = 78;
+            ChongqingWorldFinancialCenter.Width = 400;
+            ChongqingWorldFinancialCenter.Depth = 511;
+            ChongqingWorldFinancialCenter.Construct();
+            ChongqingWorldFinancialCenter.Purchase("Chongqing Worthy Land Co., Ltd.");
+            ChongqingWorldFinancialCenter.DisplayInfo();
+        }
+    }
+}
